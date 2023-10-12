b883098bc9c66666fcf61f3a5c50845a42a82b05
AlexMarin <vi.laurie@duocuc.cl>
2023-10-12 16:56:57 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RVRClimatizaciones',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
