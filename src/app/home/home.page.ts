07033f69ffbf9f107aef311e294cdeac2cc4020f
AlexMarin <vi.laurie@duocuc.cl>
2023-10-13 18:21:04 -0300

    brayan wekito

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  showMenu: boolean = false;
+  showMenu2: boolean = false;
+  showMenu3: boolean = false;
   constructor() {}
 
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+  toggleMenu2() {
+    this.showMenu2 = !this.showMenu2;
+  }
+  toggleMenu3() {
+    this.showMenu3 = !this.showMenu3;
+  }
+  closeMenu() {
+    this.showMenu = false;
+  }
+  closeMenu2() {
+    this.showMenu2 = false;
+  }
+  closeMenu3() {
+    this.showMenu3 = false;
+  }
+  opcionSeleccionada(opcion: string) {
+    // Implementa la lógica para manejar la opción seleccionada
+    console.log('Opción seleccionada:', opcion);
+  }
+  opcionSeleccionada2(opcion: string) {
+    // Implementa la lógica para manejar la opción seleccionada
+    console.log('Opción seleccionada:', opcion);
+  }
+  opcionSeleccionada3(opcion: string) {
+    // Implementa la lógica para manejar la opción seleccionada
+    console.log('Opción seleccionada:', opcion);
+  }
 }
